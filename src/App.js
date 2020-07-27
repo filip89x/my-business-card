cf8c36230abb23f2d99571a6b04dcacf192c9cf8
unknown <filip89x@gmail.com>
2020-07-27 15:37:43 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IT Engineer</h1>
+      <h2>Filip Bošković </h2>
+      <h3>filip89x@gmail.com</h3>
     </div>
   );
 }
